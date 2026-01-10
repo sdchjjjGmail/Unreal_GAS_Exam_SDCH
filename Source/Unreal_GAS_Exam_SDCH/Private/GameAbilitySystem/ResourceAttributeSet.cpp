088c067f223085cb76355d9a0cd82aad760095cd
Dongcheol <136212998+sdchGmail@users.noreply.github.com>
2026-01-10 13:17:02 +0900

    Mana Attribute 추가

---
+++ Source/Unreal_GAS_Exam_SDCH/Private/GameAbilitySystem/ResourceAttributeSet.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameAbilitySystem/ResourceAttributeSet.h"
+
+UResourceAttributeSet::UResourceAttributeSet()
+{
+}
+
+void UResourceAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+}
+
+void UResourceAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+}
---
+++ Source/Unreal_GAS_Exam_SDCH/Public/GameAbilitySystem/ResourceAttributeSet.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "GameAbilitySystem/AttributeAccessors.h"
+#include "ResourceAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_GAS_EXAM_SDCH_API UResourceAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UResourceAttributeSet();
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
+public:
+		UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UResourceAttributeSet, Mana)
+
+		UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UResourceAttributeSet, MaxMana)
+};
